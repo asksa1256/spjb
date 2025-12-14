b978d3cff3a2eae03c02058ccc2f62f6d30e44be
leesangdal <asksa1256@gmail.com>
2025-12-14 21:43:07 +0900

    chore: 업데이트 공지글 일부 수정

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Footer from "./components/ui/Footer";
 import ToTopButton from "./components/ui/ToTopButton";
 import SnowFall from "react-snowfall";
 import SnowConfigButton from "./components/feature/snow/SnowConfigButton";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import BGMPlayer from "./components/feature/bgm/BGMPlayer";
 import BGMPlayerToggleButton from "./components/feature/bgm/BGMPlayerToggleButton";
 import useLocalStorage from "./hooks/useLocalStorage";
@@ -16,8 +16,6 @@ export default function App() {
   const [snowflakeCount, setSnowflakeCount] = useState(150);
   const [showSnow, setShowSnow] = useLocalStorage("showSnow", true);
   const [showPlayer, setShowPlayer] = useLocalStorage("showPlayer", true);
-
-  useEffect(() => {}, []);
 
   return (
     <div
